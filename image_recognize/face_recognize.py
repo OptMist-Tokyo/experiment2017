e4af850f478af6d96abccd60da38c2a829ec9267
Daichi <math144377@gmail.com>
2017-04-29 10:50:27 +0900

    .gitignoreの実験・ついでに顔認識プログラムを微修正

--- image_recognize/face_recognize.py
+++ image_recognize/face_recognize.py
@@ -1,13 +1,14 @@
 
 # coding: utf-8
 
-# In[46]:
+# In[9]:
 
 import cv2
 import threading
 from datetime import datetime
+from scipy import ndimage
 
-cap = cv2.VideoCapture('movie4.MOV') # iPhoneだと1920 times 1080
+cap = cv2.VideoCapture('rawdata/Movie (6).MOV')
 RATIO = 2.0 
 
 FIRST_THREAD_COUNT = threading.activeCount() # 起動時のスレッド数（自分の環境だとなぜか5）
@@ -15,16 +16,17 @@ FIRST_THREAD_COUNT = threading.activeCount() # 起動時のスレッド数（自
 class FaceThread(threading.Thread): # スレッド処理をするクラス
     def __init__(self,frame):
         super(FaceThread, self).__init__()
-        self._cascade_path = "haarcascades/haarcascade_profileface.xml"
+        self._cascade_path = "haarcascades/haarcascade_frontalface_default.xml"
         self._frame = frame
         
     def run(self):
         self._gray = cv2.cvtColor(self._frame,cv2.COLOR_BGR2GRAY)
         self._cascade = cv2.CascadeClassifier(self._cascade_path)
-        self._face = self._cascade.detectMultiScale(self._gray)
+        self._gray = cv2.equalizeHist(self._gray)
+        self._face = self._cascade.detectMultiScale(self._gray,scaleFactor = 1.1,minNeighbors = 5,minSize = (100,100))
         
         if len(self._face) > 0:
-            #print("face is detected.")
+            print("face is detected.")
             self._color = (255,0,0)
             for self._rect in self._face:
                 cv2.rectangle(self._frame,tuple(self._rect[0:2]),tuple(self._rect[0:2] + self._rect[2:4]),self._color,thickness = 2)
@@ -41,8 +43,12 @@ while(cap.isOpened()):
     height = int(frame.shape[0])
     width = int(frame.shape[1])
     resized_frame = cv2.resize(frame,(int(width/RATIO),int(height/RATIO))) # 動画のサイズを1/RATIOにする
+    
+    # 動画の回転や切り取りを制御
+    #resized_frame = ndimage.rotate(resized_frame,270,reshape = False)
+    resized_frame = resized_frame[0:int(width/(3 * RATIO)),:]
 
-    cv2.imshow("movie",resized_frame)
+    #cv2.imshow("movie",resized_frame)
     
     #別スレッドが既に立ち上がっていなければ，別スレッド開始
     if(threading.activeCount() == FIRST_THREAD_COUNT): 
