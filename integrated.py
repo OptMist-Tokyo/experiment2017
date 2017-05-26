f6d85eb887db821292918023a24386f5b04beb28
Takumi Ishikawa <takumiishikawa94@gmail.com>
2017-05-26 15:47:41 +0900

    modify

--- integrated.py
+++ integrated.py
@@ -1,15 +1,15 @@
-# -*- coding: UTF-8 -*- 
+# -*- coding: UTF-8 -*-
 
-# -*- coding: UTF-8 -*- 
+# -*- coding: UTF-8 -*-
 
- 
+
 import cv2
-import math 
-import numpy as np 
+import math
+import numpy as np
 import os
 
 #mp4_path = './mp4/beranda.mp4'
-#print mp4_path  
+#print mp4_path
 #cap = cv2.VideoCapture(mp4_path)
 peopleinroom=0          #部屋の人数
 preexist=1              #画像内に人がいることを示すフラグ
@@ -19,13 +19,13 @@ mirror=False
 size=None
 """Capture video from camera"""
 # カメラをキャプチャする
-cap = cv2.VideoCapture(1) # 0はカメラのデバイス番号
+cap = cv2.VideoCapture(0) # 0はカメラのデバイス番号
 while(cap.isOpened()):
     #ret, frame = cap.read()
     #if ret:
         #動画内の連続する2コマを取り出す
     #    if j:
-    #        img_src1 = img_src2 
+    #        img_src1 = img_src2
     #    img_src2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[0:720,100:540]
     #    if j==0:
     #        j=1
@@ -47,7 +47,7 @@ while(cap.isOpened()):
         if size is not None and len(size) == 2:
             frame = cv2.resize(frame, size)
         if j:
-            img_src1 = img_src2 
+            img_src1 = img_src2
         img_src2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[0:100,0:600]
         #print j
         if j==0:
@@ -61,23 +61,23 @@ while(cap.isOpened()):
         if k == 27: # ESCキーで終了
             break
 
-     
+
         # 背景画像との差分を算出
         img_diff = cv2.absdiff(img_src2, img_src1)
-     
+
         # 差分を二値化
         img_diffm = cv2.threshold(img_diff, 20, 255, cv2.THRESH_BINARY)[1]
-     
-         
+
+
         # 膨張処理、収縮処理を施してマスク画像を生成
         operator = np.ones((3, 3), np.uint8)
         img_dilate = cv2.dilate(img_diffm, operator, iterations=4)
         img_mask = cv2.erode(img_dilate, operator, iterations=4)
-     
+
         # マスク画像を使って対象を切り出す
         img_dst = cv2.bitwise_and(img_src2, img_mask)
-         
-    
+
+
         #表示画像の作成
         if np.mean(img_dst)>3:      #画像内に人がいる場合
             #モーメントから人の位置を求める
@@ -90,13 +90,13 @@ while(cap.isOpened()):
                     print(peopleinroom)
                 print('Entrance',humanx,humany)
             img_dst=cv2.circle(img_dst, (humanx,humany), 20, (255, 0, 0), -1)
-            preexist=1              
+            preexist=1
         else:                       #画像内に人がいない場合
             if j0==0:
                 j0=1
                 preexist=0
                 continue
-            
+
             if preexist==1:
                 if humanx>300: #and humany<350 and humany>150:    #ドアから人が入っていった時
                     peopleinroom-=1
