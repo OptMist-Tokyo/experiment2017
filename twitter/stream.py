34081189df636159eedccaa14cd0f448ce4fdb11
24koukin <kouki240509@gmail.com>
2017-05-30 12:31:09 +0900

    add weather reply

--- twitter/stream.py
+++ twitter/stream.py
@@ -7,7 +7,7 @@ import os
 import datetime
 import tweepy
 from PIL import Image
-
+import weather
 class StreamListener(tweepy.StreamListener):
     def on_status(self, status):
         if status.in_reply_to_screen_name == myscreen_name and status.author.screen_name != myscreen_name:
@@ -23,6 +23,22 @@ class StreamListener(tweepy.StreamListener):
                 gray_img.save('gray_img.jpg')
                 message = '@' + status.author.screen_name + " 画像を変換しました\n" + time
                 api.update_with_media(filename = "gray_img.jpg", status = message, in_reply_to_screen_id = status.id)
+            #リプに対して天気を返す　日にち(3通り)と場所(2通り)
+            if 'weather' in status.text or '天気' in status.text:
+                #日にちを指定
+                if '明日' in status.text:
+                    day=1
+                elif '明後日' in status.text:
+                    day=2
+                else : #今日の天気
+                    day=0 
+                #場所を指定  
+                if '名古屋' in status.text or 'nagoya' in status.text :
+                    location=1 
+                else : #東京
+                    location=0
+                message =  '@' + status.author.screen_name + ' ' + weather.get_weather(day,location)
+                api.update_status(status = message, in_reply_to_status_id = status.id )
             else:
                 #受け取ったリプライをそのまま返す
                 message = '@' + status.author.screen_name + " " +status.text.replace('@'+myscreen_name,"") + "\n" + time
